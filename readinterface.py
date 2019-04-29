e7dec377aeee7c1b6fd2ca885ca253461fe029b7
gzscloud <you@example.com>
2019-04-29 17:32:40 +0800

    redifine interface.txt, change code

--- readinterface.py
+++ readinterface.py
@@ -1,38 +1,56 @@
+'''
+接口文件中需要考虑2点。1、文件中包含2个部分。第一部分，以[业务说明]开头，是本api文档的组织说明，调用之间的依赖等。这个部分由开发人员编写，是设计测试数据的基础。第二部分以开头[API说明]是具体的api说明。2、入库。每次有新增的api都需要全量的读入，稳定的api需要保留在数据库中。
+'''
 import re
 import dbapi
 
+### api文件分为结构和api2个部分
+filename = 'aaa.txt'
+testname = 'aaa.txt'
+struct = ''
+apis=''
+
 ### 读取文件
-#file1 = open("innerinterfaces.txt","r")
-file1 = open("inin.txt","r")
+#file1 = open(filename,"r")
+file1 = open(testname,"r")
 str1 = file1.read()
 
-### 数据处理
-# 正则就是字符串处理,"\n"被当成了字符(ASCII值为"5C 6E"),“\n2.”就无法识别为“以2.开头”，所以去掉“r'(^[0-9]+\.)'”中的“^”.
-# 正则是否带r，也有区别。有r会保留匹配的项，没有r去掉匹配的项。所以去掉“r'([0-9]+\.)'”中的“r”
+[struct, apis]=str1.split('[API说明]')
+
+### 处理api组织
+
+
+
+
+
+### 处理api
+'''
+正则就是字符串处理,"\n"被当成了字符(ASCII值为"5C 6E"),“\n2.”就无法识别为“以2.开头”，所以去掉“r'(^[0-9]+\.)'”中的“^”.
+正则是否带r，也有区别。有r会保留匹配的项，没有r去掉匹配的项。所以去掉“r'([0-9]+\.)'”中的“r”
+'''
+apikeys=[]
+apivalues=[]
 # 获取ID
-apikeys = re.findall(r'(^[0-9]+)\.', str1, re.M)
+#findall返回最短值，finditer返回最长的值，如第一条结果对应为"1.1"和"1.1.1"。"坑“
+#apikeys = listre.findall(r'(^[0-9]\.+[0-9])',  apis, re.M)
+api1 = re.finditer(r'([0-9]\.)+[0-9]+',  apis, re.M)
+for aa in api1:
+	apikeys.append(aa.group())
 
 #获取json内容
-apivalues = re.split('[0-9]+\.', str1)
+apivalues = re.split('([0-9]\.)+[0-9]+', apis)
 # 去掉list中的第一个空对象
 apivalues.pop(0)
 # utf8格式中，汉字为3个ASCII字符。去掉所有的空格(ASCII值为20)和tab键(ASCII值为09)，tab一般会变成多个空格。
 # 将中文的：替换为英文的:,将空格去掉，将行首的回车“^\n”替换为“”。
 # 各种去掉\n。
 
-# map转换为list
-def map2list(mp):
-	lst=[]
-	for ll in mp:
-		lst.append(ll)
-	return lst
-
 list1=apivalues;
 
 trans1=[('：',':'),(' ',''),('^\n',''),('(\n)+','\n'),('\n}','}'),('\n{','{'),('{\n','{'),('}\n','}'),('\n\[','['),('\n\]',']'),('\[\n','['),('\]\n',']'),(',\n',','),('\n',','),('返回数据',',返回数据')]
 for o1,t1 in trans1:
 	values_map = map(lambda _:re.sub(o1,t1,_,0,0),list1)
-	list1=map2list(values_map)
+	list1=list(values_map)
 
 ### 写入数据库
 # 建库表
